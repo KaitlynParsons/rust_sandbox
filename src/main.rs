bb021004471b57e93665a2afba55ce02747e5d10
DESKTOP-MRO5CVV\kaiti <kaitie.98@hotmail.com>
2022-01-20 17:04:07 +1000

    more learning...

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,13 @@ mod strings;
 mod tuples;
 mod arrays;
 mod vectors;
+mod conditionals;
+mod loops;
+mod functions;
+mod pointer_ref;
+mod structs;
+mod enums;
+mod cli;
 
 fn main() {
     print::run();
@@ -14,4 +21,11 @@ fn main() {
     tuples::run();
     arrays::run();
     vectors::run();
+    conditionals::run();
+    loops::run();
+    functions::run();
+    pointer_ref::run();
+    structs::run();
+    enums::run();
+    cli::run();
 }
